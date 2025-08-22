87822963aa16917b549bc565f59c09d315eacaf8
Aditi126 <adiime126@gmail.com>
2025-08-23 03:50:02 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use('/images', express.static('./upload/images'))
 app.post("/upload", upload.single('product'), (req, res)=>{
     res.json({
         success: 1,
-        iimage_url: `https://drapify-backend.onrender.com/images/${req.file.filename}`
+        image_url: `https://drapify-backend.onrender.com/images/${req.file.filename}`
     })
 })
 
